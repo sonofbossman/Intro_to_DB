29d993f225aded454ee5ea24b8050a7da337cc2d
sonofbossman <akingbadeeniola452@gmail.com>
2025-06-28 14:12:35 +0100

    Designed a MySQL database for an online bookstore. The database stores information about books, authors, customers, orders, and order details.

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,50 +1,45 @@
--- Create database
+-- Database creation
 CREATE DATABASE IF NOT EXISTS alx_book_store;
-
 USE alx_book_store;
 
--- Books: Stores information about books available in the bookstore.
-CREATE TABLE IF NOT EXISTS Books(
-  book_id INT AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(130),
-  author_id INT,
-  price DOUBLE,
-  publication_date DATE,
-  FOREIGN KEY(author_id) REFERENCES Authors(author_id)
+-- Authors table
+CREATE TABLE Authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
 );
 
-
--- Authors: Stores information about authors.
-CREATE TABLE IF NOT EXISTS Authors(
-  author_id INT AUTO_INCREMENT PRIMARY KEY,
-  author_name VARCHAR(130)
+-- Books table
+CREATE TABLE Books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    price DOUBLE NOT NULL,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
-
--- Customers: Stores information about customers.
-CREATE TABLE IF NOT EXISTS Customers(
-  customer_id INT AUTO_INCREMENT PRIMARY KEY,
-  customer_name VARCHAR(215),
-  email VARCHAR(215),
-  address TEXT
+-- Customers table
+CREATE TABLE Customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) UNIQUE NOT NULL,
+    address TEXT
 );
 
-
--- Stores information about orders placed by customers.
-CREATE TABLE IF NOT EXISTS Orders(
-  order_id INT AUTO_INCREMENT PRIMARY KEY,
-  customer_id INT,
-  order_date DATE,
-  FOREIGN KEY(customer_id) REFERENCES Customers(customer_id)
+-- Orders table
+CREATE TABLE Orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-
--- Stores information about the books included in each order.
-CREATE TABLE IF NOT EXISTS Order_Details(
-  orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
-  order_id INT,
-  book_id INT,
-  quantity DOUBLE,
-  FOREIGN KEY(order_id) REFERENCES Orders(order_id),
-  FOREIGN KEY(book_id) REFERENCES Books(book_id),
+-- Order_Details table
+CREATE TABLE Order_Details (
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
