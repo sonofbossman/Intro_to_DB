162c73ab0c4f91b8e69b8c8df0d62d8ca9c30387
sonofbossman <akingbadeeniola452@gmail.com>
2025-06-28 14:22:40 +0100

    Designed a MySQL database for an online bookstore. The database stores information about books, authors, customers, orders, and order details.

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -17,7 +17,7 @@ CREATE TABLE  Books(
   author_id INT,
   price DOUBLE NOT NULL,
   publication_date DATE,
-  FOREIGN KEY(author_id) REFERENCES Authors(author_id)
+  FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
 -- Customers: Stores information about customers.
@@ -34,7 +34,7 @@ CREATE TABLE  Orders(
   order_id INT AUTO_INCREMENT PRIMARY KEY,
   customer_id INT,
   order_date DATE,
-  FOREIGN KEY(customer_id) REFERENCES Customers(customer_id)
+  FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 
@@ -44,6 +44,6 @@ CREATE TABLE Order_Details(
   order_id INT,
   book_id INT,
   quantity DOUBLE,
-  FOREIGN KEY(order_id) REFERENCES Orders(order_id),
-  FOREIGN KEY(book_id) REFERENCES Books(book_id),
+  FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+  FOREIGN KEY (book_id) REFERENCES Books(book_id),
 );
