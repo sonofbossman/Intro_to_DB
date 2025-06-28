8a5a8758689244579552266c713f70247760c854
sonofbossman <akingbadeeniola452@gmail.com>
2025-06-28 14:01:31 +0100

    Designed a MySQL database for an online bookstore. The database stores information about books, authors, customers, orders, and order details.

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,23 +1,23 @@
 -- Books: Stores information about books available in the bookstore.
-CREATE TABLE IF NOT EXISTS books(
+CREATE TABLE IF NOT EXISTS Books(
   book_id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(130),
   author_id INT,
   price DOUBLE,
   publication_date DATE,
-  FOREIGN KEY(author_id) REFERENCES authors(author_id)
+  FOREIGN KEY(author_id) REFERENCES Authors(author_id)
 )
 
 
 -- Authors: Stores information about authors.
-CREATE TABLE IF NOT EXISTS authors(
+CREATE TABLE IF NOT EXISTS Authors(
   author_id INT AUTO_INCREMENT PRIMARY KEY,
   author_name VARCHAR(130)
 )
 
 
 -- Customers: Stores information about customers.
-CREATE TABLE IF NOT EXISTS customers(
+CREATE TABLE IF NOT EXISTS Customers(
   customer_id INT AUTO_INCREMENT PRIMARY KEY,
   customer_name VARCHAR(215),
   email VARCHAR(215),
@@ -26,20 +26,20 @@ CREATE TABLE IF NOT EXISTS customers(
 
 
 -- Stores information about orders placed by customers.
-CREATE TABLE IF NOT EXISTS orders(
+CREATE TABLE IF NOT EXISTS Orders(
   order_id INT AUTO_INCREMENT PRIMARY KEY,
   customer_id INT,
   order_date DATE,
-  FOREIGN KEY(customer_id) REFERENCES customers(customer_id)
+  FOREIGN KEY(customer_id) REFERENCES Customers(customer_id)
 )
 
 
 -- Stores information about the books included in each order.
-CREATE TABLE IF NOT EXISTS order_details(
+CREATE TABLE IF NOT EXISTS Order_Details(
   orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
   order_id INT,
   book_id INT,
   quantity DOUBLE,
-  FOREIGN KEY(order_id) REFERENCES orders(order_id),
-  FOREIGN KEY(book_id) REFERENCES books(book_id),
+  FOREIGN KEY(order_id) REFERENCES Orders(order_id),
+  FOREIGN KEY(book_id) REFERENCES Books(book_id),
 )
