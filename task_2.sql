da4511eaba0b2de6b29466d426b8b4485e07b074
sonofbossman <akingbadeeniola452@gmail.com>
2025-06-28 19:06:35 +0100

    Create Your Magical Tables

---
+++ task_2.sql
@@ -0,0 +1,46 @@
+USE alx_book_store;
+
+-- Authors: Stores information about authors.
+CREATE TABLE  Authors(
+  author_id INT AUTO_INCREMENT PRIMARY KEY,
+  author_name VARCHAR(215) NOT NULL
+);
+
+
+-- Books: Stores information about books available in the bookstore.
+CREATE TABLE  Books(
+  book_id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(130) NOT NULL,
+  author_id INT,
+  price DOUBLE NOT NULL,
+  publication_date DATE,
+  FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+-- Customers: Stores information about customers.
+CREATE TABLE  Customers(
+  customer_id INT AUTO_INCREMENT PRIMARY KEY,
+  customer_name VARCHAR(215),
+  email VARCHAR(215),
+  address TEXT
+);
+
+
+-- Stores information about orders placed by customers.
+CREATE TABLE  Orders(
+  order_id INT AUTO_INCREMENT PRIMARY KEY,
+  customer_id INT,
+  order_date DATE,
+  FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+
+-- Stores information about the books included in each order.
+CREATE TABLE Order_Details(
+  orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+  order_id INT,
+  book_id INT,
+  quantity DOUBLE,
+  FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+  FOREIGN KEY (book_id) REFERENCES Books(book_id),
+);
